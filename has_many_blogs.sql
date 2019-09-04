2b17ca7e8854c0d8b35073270381f53b224d1a4b
Janelle K Hirano <jaahnelle@gmail.com>
2019-09-03 22:52:11 -1000

    finsihed up til no.6, cant see the results of the queries yet

--- has_many_blogs.sql
+++ has_many_blogs.sql
@@ -2,38 +2,35 @@ DROP TABLE IF EXISTS users;
 
 CREATE TABLE users
 (
-    id SERIAL PRIMARY KEY NOT NULL,
-    username VARCHAR(90) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    username VARCHAR(90),
     first_name VARCHAR (90) DEFAULT NULL,
     last_name VARCHAR (90)DEFAULT NULL,
-    created_at timestamp NOT NULL DEFAULT now(),
-    updated_at timestamp NOT NULL DEFAULT now
-    ()
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now()
 );
 
 DROP TABLE IF EXISTS posts;
 
 CREATE TABLE posts
 (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     title VARCHAR(180) DEFAULT NULL,
     url VARCHAR(510) DEFAULT NULL,
     content text DEFAULT NULL,
-    created_at timestamp NOT NULL DEFAULT now
-    (),
-    updated_at timestamp NOT NULL DEFAULT now
-    (),
-    user_id INT REFERENCES users(id) NOT NULL
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now(),
+    user_id INTEGER REFERENCES users(id)
 );
 
 DROP TABLE IF EXISTS comments;
 
 CREATE TABLE comments
 (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     body VARCHAR(510) DEFAULT NULL,
-    created_at timestamp NOT NULL DEFAULT now(),
-    updated_at timestamp NOT NULL DEFAULT now(),
-    user_id INT REFERENCES users(id) NOT NULL,
-    post_id INT REFERENCES posts(id) NOT NULL
+    created_at timestamp DEFAULT now(),
+    updated_at timestamp DEFAULT now(),
+    user_id INTEGER REFERENCES users(id),
+    post_id INTEGER REFERENCES posts(id)
 );
---
+++ joins.sql
@@ -0,0 +1,59 @@
+-- 1
+SELECT *
+FROM users;
+
+-- 2
+SELECT *
+FROM posts
+WHERE user_id=100;
+
+-- 3
+-- Create a query to get all posts fields, the user's first name, and the user's last name, from the posts table where the user's id is 200
+SELECT posts.*, users.first_name, users.last_name
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE user_id=200;
+
+-- 4
+-- Create a query to get all posts fields, and the user's username, from the posts table where the user's first name is 'Norene' and the user's last_name is 'Schmitt'
+SELECT posts.*, users.username
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE users.first_name="Norene" 
+AND users.last_name="Schmitt";
+
+-- 5
+-- Create a query to get usernames from the users table where the user has created a post after January 1, 2015
+SELECT users.username
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE posts.created_at > "2015-1-1";
+
+-- 6
+-- Create a query to get the post title, post content, and user's username where the user who created the post joined before January 1, 2015
+SELECT posts.title, posts.content, users.username
+FROM posts
+INNER JOIN users ON posts.user_id = users.id
+WHERE users.created_at < "2015-1-1";
+
+-- 7
+-- Create a query to get the all rows in the comments table, showing post title (aliased as 'Post Title'), and the all the comment's fields
+-- SELECT comments.*
+
+-- 8
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created before January 1, 2015
+
+-- 9
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the post was created after January 1, 2015
+
+-- 10
+-- Create a query to get the all rows in the comments table, showing post title (aliased as post_title), post url (ailased as post_url), and the comment body (aliased as comment_body) where the comment body contains the word 'USB'
+
+-- 11
+-- Create a query to get the post title (aliased as post_title), first name of the author of the post, last name of the author of the post, and comment body (aliased to comment_body), where the comment body contains the word 'matrix' ( should have 855 results )
+
+-- 12
+-- Create a query to get the first name of the author of the comment, last name of the author of the comment, and comment body (aliased to comment_body), where the comment body contains the word 'SSL' and the post content contains the word 'dolorum' ( should have 102 results )
+
+-- 13
+-- Create a query to get the first name of the author of the post (aliased to post_author_first_name), last name of the author of the post (aliased to post_author_last_name), the post title (aliased to post_title), username of the author of the comment (aliased to comment_author_username), and comment body (aliased to comment_body), where the comment body contains the word 'SSL' or 'firewall' and the post content contains the word 'nemo' ( should have 218 results )
